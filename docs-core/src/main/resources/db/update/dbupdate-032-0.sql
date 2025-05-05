75e1fb5a0c96736c9495dbbba3f51eea27c1d148
Nefelibata-Zhu <12211214@mail.sustech.edu.cn>
2025-05-05 23:24:03 +0800

    删除dbupdate-032-0.sql文件

--- docs-core/src/main/resources/db/update/dbupdate-032-0.sql
+++
@@ -1,6 +0,0 @@
-create cached table T_USER_REGISTRATION_REQUEST (URR_ID_C varchar(36) not null, URR_USERNAME_C varchar(50) not null, URR_PASSWORD_C varchar(255) not null, URR_EMAIL_C varchar(100) not null, URR_CREATEDATE_D timestamp not null, URR_STATUS_C varchar(50) not null, primary key (URR_ID_C));
-
-create index IDX_URR_USERNAME_C on T_USER_REGISTRATION_REQUEST (URR_USERNAME_C);
-create index IDX_URR_STATUS_C on T_USER_REGISTRATION_REQUEST (URR_STATUS_C);
-
-update T_CONFIG set CFG_VALUE_C = '32' where CFG_ID_C = 'DB_VERSION';
